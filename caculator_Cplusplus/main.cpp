49dd6ef2100dccdc6f608001c8eb37ab84a48d9b
seafeawea <452576504@qq.com>
2015-12-03 18:26:22 +0800

    Initial Commit

---
+++ caculator_Cplusplus/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  caculator_Cplusplus
+//
+//  Created by XZ on 15/12/3.
+//  Copyright © 2015年 XZ. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
